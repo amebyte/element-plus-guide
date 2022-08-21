dacc9b038b9ac0d8c805c1aad6e21329cb4a0dbb
unknown <416212316@qq.com>
2022-08-21 15:51:21 +0800

    bem

--- packages/utils/create.ts
+++ packages/utils/create.ts
@@ -1,6 +1,6 @@
 // block 代码块 element 元素 modifier 装饰 state 状态
 
-function _ben(prefixName: string, blockSuffix: string, element: string, modifier: string) {
+function _bem(prefixName: string, blockSuffix: string, element: string, modifier: string) {
     if(blockSuffix) {
         prefixName += `-${blockSuffix}`
     }
@@ -14,9 +14,24 @@ function _ben(prefixName: string, blockSuffix: string, element: string, modifier
 }
 
 function createBEM(prefixName: string) {
-    const b = (blockSuffix: string = '') => _ben(prefixName, blockSuffix, '', '')
+    const b = (blockSuffix: string = '') => _bem(prefixName, blockSuffix, '', '')
+    const e = (element: string = '') => element ? _bem(prefixName, '', element, '') : ''
+    const m = (modifier: string = '') => modifier ? _bem(prefixName, '', '', modifier) : ''
+    const be = (blockSuffix: string = '', element: string = '') => blockSuffix && element ? _bem(prefixName, blockSuffix, element, '') : ''
+    const bm = (blockSuffix: string = '', modifier: string = '') => blockSuffix && modifier ? _bem(prefixName, blockSuffix, '', modifier) : ''
+    const em = (element: string = '', modifier: string = '') => element && modifier ? _bem(prefixName, '', element, modifier) : ''
+    const bem = (blockSuffix: string = '', element: string = '', modifier: string = '') => blockSuffix && element && modifier ? _bem(prefixName, blockSuffix, element, modifier) : ''
+    
+    const is = (name:string, state) => (state ? `is-${name}` : ``)
     return {
-        b
+        b,
+        e,
+        m,
+        be,
+        bm,
+        em,
+        bem,
+        is
     }
 }
 
