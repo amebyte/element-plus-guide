492af15ddc4e410ff23dc0081a6040287554bcac
unknown <416212316@qq.com>
2022-08-21 18:11:35 +0800

    typeProps

---
+++ packages/components/icon/src/icon.ts
@@ -0,0 +1,10 @@
+// 这里准备组件相关的属性 和 ts 的类型
+
+import { ExtractPropTypes, PropType } from 'vue'
+
+export const iconProps = {
+    color: String,
+    size: [Number, String] as PropType<number | string>
+} as const
+
+type IconProps = ExtractPropTypes<typeof iconProps>
