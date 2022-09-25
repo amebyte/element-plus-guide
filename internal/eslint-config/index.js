5b9c7c6a71ca443a527acb36d540fcf07b064c77
unknown <416212316@qq.com>
2022-09-26 01:31:57 +0800

    fix

--- internal/eslint-config/index.js
+++ internal/eslint-config/index.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   // extends 多个模块，如果规则冲突，位置靠后的包将覆盖前面的。rules 中的规则相同，并且优先级恒定高于 extends
   extends: [
     'eslint:recommended',
-    'plugin:import/recommended',
+    'plugin:import/recommended', // 插件中的 extends
     'plugin:eslint-comments/recommended',
     'plugin:jsonc/recommended-with-jsonc',
     'plugin:markdown/recommended',
