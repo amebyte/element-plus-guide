cddda1cfa0bbadfc6f952c776723f555294a7126
unknown <416212316@qq.com>
2022-11-12 15:28:32 +0800

    fix

--- packages/hooks/use-namespace/index.ts
+++ packages/hooks/use-namespace/index.ts
@@ -2,7 +2,7 @@ import { computed, unref } from 'vue'
 // 命名前缀
 export const defaultNamespace = 'el'
 const statePrefix = 'is-'
-
+// BEM 命名字符拼接函数
 const _bem = (
   namespace: string,
   block: string,
@@ -10,13 +10,17 @@ const _bem = (
   element: string,
   modifier: string
 ) => {
+  // 默认是 Block
   let cls = `${namespace}-${block}`
+  // Block 前缀，也就是 Block 里面还有 Block，例如：el-form 下面还有一个 el-form-item
   if (blockSuffix) {
     cls += `-${blockSuffix}`
   }
+  // 元素
   if (element) {
     cls += `__${element}`
   }
+  // 修改器
   if (modifier) {
     cls += `--${modifier}`
   }
