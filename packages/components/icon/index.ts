40463a89fd86b203acf4006983742597eb181e5c
unknown <416212316@qq.com>
2022-11-20 21:56:40 +0800

    fix

--- packages/components/icon/index.ts
+++ packages/components/icon/index.ts
@@ -1,14 +1,12 @@
 // 用来整合组件的，最终实现导出组件
 
-// import { withInstall } from '@cobyte-ui/utils'
-import ElIcon from './src/icon.vue'
-// const Icon = withInstall(_Icon)
+import { withInstall } from '@cobyte-ui/utils'
+import Icon from './src/icon.vue'
+// 通过 withInstall 方法给 Icon 添加了一个 install 方法
+const ElIcon = withInstall(Icon)
 // 可以通过 app.use 来使用，也可以通过 import 方式单独使用
 export default ElIcon
-// export * from './src/icon'
-
-// const b = 123
-// b = 56
+export * from './src/icon'
 
 // 这里添加的类型，可以在模板里面被解析
 // declare module 'vue' {
--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -1,19 +1,9 @@
 import type { Plugin } from 'vue'
+// 通过 Vue 提供的 Plugin 类型和传进来的组件类型 T 的集合进行确定我们的组件类型具有 Plugin 类型方法，如 install 方法
 export type SFCWithInstall<T> = T & Plugin
-export const withInstall = <T, E extends Record<string, any>>(
-  main: T,
-  extra?: E
-) => {
-  ;(main as SFCWithInstall<T>).install = (app): void => {
-    for (const comp of [main, ...Object.values(extra ?? {})]) {
-      app.component(comp.name, comp)
-    }
+export const withInstall = <T>(comp: T) => {
+  ;(comp as SFCWithInstall<T>).install = function (app) {
+    app.component('ElIcon', comp as SFCWithInstall<T>)
   }
-
-  if (extra) {
-    for (const [key, comp] of Object.entries(extra)) {
-      ;(main as any)[key] = comp
-    }
-  }
-  return main as SFCWithInstall<T> & E
+  return comp as SFCWithInstall<T>
 }
