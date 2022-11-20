7100e83b7a3e64084b3191475dcd8b008f4daffd
unknown <416212316@qq.com>
2022-11-20 22:11:38 +0800

    element plus

--- play/main.ts
+++ play/main.ts
@@ -1,11 +1,24 @@
 import { createApp } from 'vue'
-// import CIcon from '@cobyte-ui/components/icon'
-// import '@cobyte-ui/theme-chalk/src/index.scss'
+import ElIcon from '@cobyte-ui/components/icon'
+import '@cobyte-ui/theme-chalk/src/index.scss'
 import App from './src/App.vue'
-
-// const plugins = [CIcon]
+// 组件库
+const components = [ElIcon]
+// 是否已安装标识
+const INSTALLED_KEY = Symbol('INSTALLED_KEY')
+// 组件库插件
+const ElementPlus = {
+  install(app: any) {
+    // 如果该组件库已经安装过了，则不进行安装
+    if (app[INSTALLED_KEY]) return
+    // 将标识值设置为 true，表示已经安装了
+    app[INSTALLED_KEY] = true
+    // 循环组件库中的每个组件进行安装
+    components.forEach((c) => app.use(c))
+  },
+}
 
 const app = createApp(App)
-// plugins.forEach((plugin) => app.use(plugin))
-
+// 安装组件库
+app.use(ElementPlus)
 app.mount('#app')
