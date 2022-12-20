3367411b347931358821ae23232b52468a03da5f
unknown <416212316@qq.com>
2022-12-20 23:23:25 +0800

    导出ButtonGroup

--- packages/components/button/index.ts
+++ packages/components/button/index.ts
@@ -1,11 +1,12 @@
-import { withInstall } from '@cobyte-ui/utils'
+import { withInstall, withNoopInstall } from '@cobyte-ui/utils'
 import Button from './src/button.vue'
 import ButtonGroup from './src/button-group.vue'
 
-// 通过 withInstall 方法给 Button 添加了一个 Vue3 插件所需的 install 方法
-const ElButton = withInstall(Button)
-// 通过 withInstall 方法给 ButtonGroup 添加了一个 Vue3 插件所需的 install 方法
-export const ElButtonGroup = withInstall(ButtonGroup)
+// 通过 withInstall 方法给 Button 添加了一个 install 方法
+const ElButton = withInstall(Button, { ButtonGroup })
+
+// 导出 ButtonGroup 组件
+export const ElButtonGroup = withNoopInstall(ButtonGroup)
 // 可以通过 app.use 来使用，也可以通过 import 方式单独使用
 export default ElButton
 export * from './src/button'
