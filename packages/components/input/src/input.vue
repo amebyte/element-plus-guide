3bdd4bc7432820cfaab8db81d84d02194fe6a1da
unknown <416212316@qq.com>
2023-02-19 22:58:39 +0800

    input

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,17 +1,44 @@
 <template>
   <div :class="nsInput.b()">
-    <input :type="type" />
+    <input :type="type" @input="handleInput" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed, nextTick, onMounted, shallowRef } from 'vue'
 import { useNamespace } from '@cobyte-ui/hooks'
+import { UPDATE_MODEL_EVENT } from '@cobyte-ui/constants'
 import { inputEmits, inputProps } from './input'
+
+type TargetElement = HTMLInputElement | HTMLTextAreaElement
+
 defineOptions({
   name: 'ElInput',
 })
-defineProps(inputProps)
-defineEmits(inputEmits)
+const props = defineProps(inputProps)
+const emit = defineEmits(inputEmits)
 
 const nsInput = useNamespace('input')
+
+const input = shallowRef<HTMLInputElement>()
+const _ref = computed(() => input.value)
+const nativeInputValue = computed(() =>
+  props.modelValue ? '' : String(props.modelValue)
+)
+const setNativeInputValue = () => {
+  const input = _ref.value
+  if (!input || input.value === nativeInputValue.value) return
+  input.value = nativeInputValue.value
+}
+
+const handleInput = async (event: Event) => {
+  const { value } = event.target as TargetElement
+  emit(UPDATE_MODEL_EVENT, value)
+  await nextTick()
+  setNativeInputValue()
+}
+
+onMounted(() => {
+  setNativeInputValue()
+})
 </script>
--- play/src/App.vue
+++ play/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <el-input />
-  <br />
-  <br />
-  <br />
-  <input :value="state" name="username" @change="onChange" />
-  <br />
-  <br />
-  <el-button type="primary" @click="onSubmit">提交</el-button>
-  <br />
-  <br />
-  state 值：{{ state }}
+  <el-input v-model="state" />
 </template>
-<script setup>
-import { ref } from 'vue'
-// 编辑时的默认值
-const state = ref('稀土掘金')
-const onChange = (e) => {
-  state.value = e.target.value
-}
-// 提交方法
-const onSubmit = () => {
-  console.log('提交的数据：')
-  console.log('state值', state.value)
-}
+
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+
+const state = ref('')
+watch(
+  () => state.value,
+  (newVal) => {
+    console.log(newVal)
+  }
+)
 </script>
