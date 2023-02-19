475c9bc22191c9f32977d1d018f38072027c1550
unknown <416212316@qq.com>
2023-02-19 17:55:55 +0800

    fix input

--- packages/components/input/src/input.ts
+++ packages/components/input/src/input.ts
@@ -6,7 +6,7 @@ import type Input from './input.vue'
 // 定义 props
 export const inputProps = {
   modelValue: {
-    type: [String, Number, Object],
+    type: [String, Number, Object] as PropType<string | number | object>,
     default: '',
   },
   type: {
@@ -20,10 +20,6 @@ export type InputProps = ExtractPropTypes<typeof inputProps>
 
 export const inputEmits = {
   [UPDATE_MODEL_EVENT]: (value: string) => isString(value),
-  input: (value: string) => isString(value),
-  change: (value: string) => isString(value),
-  focus: (evt: FocusEvent) => evt instanceof FocusEvent,
-  blur: (evt: FocusEvent) => evt instanceof FocusEvent,
 }
 
 export type InputEmits = typeof inputEmits
---
+++ packages/components/input/src/input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="nsInput.b()">
+    <input :type="type" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useNamespace } from '@cobyte-ui/hooks'
+import { inputEmits, inputProps } from './input'
+defineOptions({
+  name: 'ElInput',
+})
+defineProps(inputProps)
+defineEmits(inputEmits)
+
+const nsInput = useNamespace('input')
+</script>
