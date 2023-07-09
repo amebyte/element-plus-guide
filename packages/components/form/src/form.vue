efc3b1326d09772f5713cbd1ef04e9ca7294826d
unknown <416212316@qq.com>
2023-07-09 13:05:03 +0800

    Async Validation Error

--- packages/components/form/src/form.vue
+++ packages/components/form/src/form.vue
@@ -6,13 +6,17 @@ import { computed, provide, reactive, toRefs } from 'vue'
 import { useNamespace } from '@cobyte-ui/hooks'
 import { formContextKey } from '@cobyte-ui/tokens'
 import { formEmits, formProps } from './form'
+
+defineOptions({
+  name: 'ElForm',
+})
 const props = defineProps(formProps)
 const emit = defineEmits(formEmits)
 const ns = useNamespace('form')
 const formClasses = computed(() => {
   return [ns.b()]
 })
-
+console.log('props', props)
 provide(
   formContextKey,
   reactive({
