ed36a4c3b0625f2503c8ae0cc5ef91381a1e101f
unknown <416212316@qq.com>
2023-07-16 23:26:44 +0800

    form-item

--- packages/components/form1/src/form-item.vue
+++ packages/components/form1/src/form-item.vue
@@ -12,11 +12,11 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { provide, reactive, ref, toRefs } from 'vue'
+import { computed, provide, reactive, ref, toRefs } from 'vue'
 import { useNamespace } from '@cobyte-ui/hooks'
 import { formItemContextKey } from '@cobyte-ui/tokens/form'
 import { formItemProps } from './form-item'
-import type { FormItemContext } from '@cobyte-ui/tokens/form'
+import type { FormItemContext, FormItemRule } from '@cobyte-ui/tokens/form'
 defineOptions({
   name: 'ElFormItem',
 })
@@ -24,8 +24,31 @@ const props = defineProps(formItemProps)
 const ns = useNamespace('form-item')
 const validateMessage = ref('')
 
+const _rules = computed(() => {
+  const rules: FormItemRule[] = props.rules
+    ? Array.isArray(props.rules)
+      ? props.rules
+      : [props.rules]
+    : []
+  return rules
+})
+
+const getFilteredRule = (trigger: string) => {
+  const rules = _rules.value
+  return rules.filter((rule) => {
+    // 这种情况全部校验
+    if (!rule.trigger || !trigger) return true
+    if (Array.isArray(rule.trigger)) {
+      return rule.trigger.includes(trigger)
+    } else {
+      return rule.trigger === trigger
+    }
+  })
+}
+
 const validate: FormItemContext['validate'] = async (trigger) => {
-  console.log('trigger', trigger)
+  const rules = getFilteredRule(trigger)
+  console.log('trigger', trigger, rules)
 }
 
 const context: FormItemContext = reactive({
--- play/src/App.vue
+++ play/src/App.vue
@@ -2,13 +2,19 @@
   <el-form>
     <el-form-item
       label="用户名："
-      :rules="{
-        username: {
+      :rules="[
+        {
           required: true,
           message: '请输入用户名',
           trigger: 'blur',
         },
-      }"
+        {
+          min: 6,
+          max: 10,
+          message: '用户名长度最少6位且不能超过10位',
+          trigger: 'change',
+        },
+      ]"
     >
       <el-input v-model="data.username" />
     </el-form-item>
