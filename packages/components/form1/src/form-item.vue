48d36e4c559d543e272192e2e5052156b4d0136a
Coboy <416212316@qq.com>
2023-07-18 12:01:40 +0800

    form-item validate

--- packages/components/form1/src/form-item.vue
+++ packages/components/form1/src/form-item.vue
@@ -12,24 +12,41 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { computed, provide, reactive, ref, toRefs } from 'vue'
+import { computed, inject, provide, reactive, ref, toRefs } from 'vue'
+import AsyncValidator from 'async-validator'
 import { useNamespace } from '@cobyte-ui/hooks'
-import { formItemContextKey } from '@cobyte-ui/tokens/form'
+import { formContextKey, formItemContextKey } from '@cobyte-ui/tokens/form'
 import { formItemProps } from './form-item'
-import type { FormItemContext, FormItemRule } from '@cobyte-ui/tokens/form'
+import type {
+  FormItemContext,
+  FormItemRule,
+  FormValidateFailure,
+} from '@cobyte-ui/tokens/form'
+import type { Arrayable } from '@cobyte-ui/utils'
 defineOptions({
   name: 'ElFormItem',
 })
 const props = defineProps(formItemProps)
+const formContext = inject(formContextKey, undefined)
 const ns = useNamespace('form-item')
 const validateMessage = ref('')
 
+// 把校验规则统一处理成数组
+const ensureArray = (rules: Arrayable<FormItemRule> | undefined) => {
+  return rules ? (Array.isArray(rules) ? rules : [rules]) : []
+}
+
 const _rules = computed(() => {
-  const rules: FormItemRule[] = props.rules
-    ? Array.isArray(props.rules)
-      ? props.rules
-      : [props.rules]
-    : []
+  const rules: FormItemRule[] = ensureArray(props.rules)
+  const formRules = formContext?.rules
+  // 当 Form 组件的 rules 存在并且 Form-item 的 prop 属性也需要存在，才进行规则合并
+  if (formRules && props.prop) {
+    const _rules = formRules[props.prop]
+    if (_rules) {
+      // 在合并前同样通过 ensureArray 函数统一处理成数组
+      rules.push(...ensureArray(_rules))
+    }
+  }
   return rules
 })
 
@@ -46,9 +63,37 @@ const getFilteredRule = (trigger: string) => {
   })
 }
 
+// 校验失败的处理
+const onValidationFailed = (error) => {
+  const { errors } = error
+  validateMessage.value = errors
+    ? errors?.[0]?.message ?? `${props.prop} is required`
+    : ''
+}
+// 校验成功的处理
+const onValidationSucceeded = () => {
+  validateMessage.value = ''
+}
+
 const validate: FormItemContext['validate'] = async (trigger) => {
   const rules = getFilteredRule(trigger)
-  console.log('trigger', trigger, rules)
+  // 获取 Form-item 的 prop 属性
+  const modelName = props.prop!
+  // 获取 Form 组件的 model 的数据源
+  const model = formContext?.model!
+  // 声明校验实例对象
+  const validator = new AsyncValidator({
+    [modelName]: rules,
+  })
+  return validator
+    .validate({ [modelName]: model[modelName] }, { firstFields: true })
+    .then(() => {
+      onValidationSucceeded()
+    })
+    .catch((err: any) => {
+      onValidationFailed(err)
+      return Promise.reject(err)
+    })
 }
 
 const context: FormItemContext = reactive({
