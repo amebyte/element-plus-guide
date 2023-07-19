5a4d2e57928374165c9f2fdebb52ac7f5902d6dd
Coboy <416212316@qq.com>
2023-07-19 11:17:30 +0800

    form

--- packages/components/form1/src/form.vue
+++ packages/components/form1/src/form.vue
@@ -6,16 +6,23 @@ import { provide, reactive, toRefs } from 'vue'
 import { useNamespace } from '@cobyte-ui/hooks'
 import { formContextKey } from '@cobyte-ui/tokens'
 import { formProps } from './form'
+import type { FormContext, FormItemContext } from '@cobyte-ui/tokens'
 defineOptions({
   name: 'ElForm',
 })
 const props = defineProps(formProps)
+const fields: FormItemContext[] = []
 const ns = useNamespace('form')
+
+const addField: FormContext['addField'] = (context) => {
+  fields.push(context)
+}
 
 provide(
   formContextKey,
   reactive({
     ...toRefs(props),
+    addField,
   })
 )
 </script>
